31f27ef40928410a518970bd276a73ef42c1c59b
Fl0ri4n D3v <52041377+Fl0ri4nDev@users.noreply.github.com>
2021-05-17 22:53:33 +0200

    add Overpass API URL

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+var myJSON = "";
+
 function displayResponse(myResponse) {
-  document.getElementById("app").innerHTML = JSON.stringify(myResponse);
+  //document.getElementById("app").innerHTML = JSON.stringify(myResponse);
+  document.getElementById("app").innerHTML = myResponse;
+}
+
+function parseJSONResponse(myResponse) {
+  myJSON = JSON.parse(myResponse);
+
+  // document.getElementById("app").innerHTML =  document.getElementById("app").innerHTML+ myResponse;
+  // var tmp=myJSON.getElementsByTagName('way')[0];
+  // document.getElementById("url").innerHTML = tmp;
 }
 
 function consoleResponse(myResponse) {
@@ -10,27 +21,37 @@ function fetchJSON(url, functionTraitement) {
   var myreturn;
 
   fetch(url)
-    .then((response) => response.json())
+    .then((response) => response.text())
     .then((data) => (myreturn = data))
     .then(() => functionTraitement(myreturn))
 
     .catch(function (error) {
-      console.log(error);
+      console.log("erreur :" + error);
     });
 }
 
-var url_base = "http://router.project-osrm.org/match/v1/bike/";
+//chemin Vincennes = 48.83521631227285,2.4347042284819675
+//route Nation : 48.84645272792164,2.400429913877873
+var url_overpass_coordinates = "48.83521631227285,2.4347042284819675";
+var url_overpass_base = "https://overpass-api.de/api/interpreter?data=";
+var url_overpass_data =
+  "[out:json];(way(around:5," + url_overpass_coordinates + ");>;);out;";
+var url_overpass = url_overpass_base + url_overpass_data;
+
+//var url_base = "https://router.project-osrm.org/match/v1/bike/";
 //var url_coordinate='2.39998,48.84674;2.400429913877873,48.84645272792164';
-var url_coordinate =
-  "2.4067760204071087,48.845358350231734;2.406598994612126,48.84525950203694";
 
-var url_parameters = "?steps=true&geometries=geojson&annotations=true";
+//var url_coordinate ="2.4067760204071087,48.845358350231734;2.406598994612126,48.84525950203694";
 
-var url_full = url_base + url_coordinate + url_parameters;
+var url_parameters = "?steps=false&geometries=geojson&annotations=true";
 
+var url_full;
+//url_full= url_base + url_coordinate + url_parameters;
+url_full = url_overpass;
 document.getElementById("url").innerHTML = url_full;
 
-fetchJSON(url_full, displayResponse);
+fetchJSON(url_full, parseJSONResponse);
+document.getElementById("app").innerHTML = myJSON;
 
 //fetchJSON("https://jsonplaceholder.typicode.com/posts", displayResponse);
 //fetchJSON("https://jsonplaceholder.typicode.com/posts", consoleResponse);
